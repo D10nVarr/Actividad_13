7328d017c24c93c2de039822283065112cb2fdc9
diegobarr <d1390xd.v@gmail.com>
2025-08-04 20:29:25 -0600

    Pruebas de funciones con el menu

--- gestorAcademico.py
+++ gestorAcademico.py
@@ -17,11 +17,16 @@ def agregar_notas():
     notas={}
     id_buscar=input("Ingrese su ID de estudiante: ")
     if id_buscar in estudiantes:
+        aprobado="Perdido"
         curso_nom=str(input("Ingrese del curso a añadir: "))
         nota_curso=int(input(f"Ingrese la nota de {curso_nom}: : "))
 
+        if nota_curso>=61:
+            aprobado="Aprobado"
+
         notas[curso_nom]={
             "Nota:":nota_curso,
+            "Aprobado":aprobado,
         }
 
         estudiantes[id_buscar]={
@@ -29,8 +34,25 @@ def agregar_notas():
         }
 
 print("---PORTAL ACADÉMICO---")
-print("1. Agregar estudiante")
+print("\n1. Agregar estudiante")
 print("2. Agregar curso con nota")
+print("3. Consultar estudiante")
+print("4. Calcular promedio")
+print("5. Verificación de aprobación")
+print("6. Mostrar todos los registros")
+print("7. Salir")
+
+opcion=input("Seleccione una opción: ")
+
+match opcion:
+    case "1":
+        print("\nEscogio 'Agregar estudiantes' ")
+        registrar_estudiante()
+    case "2":
+        print("\nEscogio 'Agregar cursos con nota' ")
+        agregar_notas()
+    case "3":
+        print("\nEscogio 'Consultar estudiantes' ")
 
 
 
