47af5233a32961dc9bc432966fa5ce4561647cb5
diegobarr <d1390xd.v@gmail.com>
2025-08-04 22:09:41 -0600

    Consulta de estudiantes completa

--- gestorAcademico.py
+++ gestorAcademico.py
@@ -10,49 +10,82 @@ def registrar_estudiante():
         "ID":id_est,
         "Nombre":nom_est,
         "Carrera":carrera_progra,
+        "Curso":{}
     }
 
-def agregar_notas():
+def todo_notas():
+#Agregador de notas y verificador de aprobación
     global estudiantes
     notas={}
     id_buscar=input("Ingrese su ID de estudiante: ")
+    print(f"Bienvenido {estudiantes[id_buscar]['Nombre']}")
+
     if id_buscar in estudiantes:
-        aprobado="Perdido"
+        val_aprobado="Perdido"
+        todas_notas=[]
         curso_nom=str(input("Ingrese del curso a añadir: "))
         nota_curso=int(input(f"Ingrese la nota de {curso_nom}: : "))
+        todas_notas.append(nota_curso)
 
         if nota_curso>=61:
-            aprobado="Aprobado"
+            val_aprobado="Aprobado"
 
         notas[curso_nom]={
-            "Nota:":nota_curso,
-            "Aprobado":aprobado,
+            "Nota":nota_curso,
+            "Valor_aprobacion":val_aprobado,
+            #"Suma":sum(todas_notas)
         }
 
-        estudiantes[id_buscar]={
-            "Curso":notas,
-        }
+        estudiantes[id_buscar]["Curso"].update(notas)
+    else:
+        print("No se ha encontrado al estudiante")
 
-print("---PORTAL ACADÉMICO---")
-print("\n1. Agregar estudiante")
-print("2. Agregar curso con nota")
-print("3. Consultar estudiante")
-print("4. Calcular promedio")
-print("5. Verificación de aprobación")
-print("6. Mostrar todos los registros")
-print("7. Salir")
 
-opcion=input("Seleccione una opción: ")
+def consultar_est(buscador):
+#Tambien como calculador de promedio
 
-match opcion:
-    case "1":
-        print("\nEscogio 'Agregar estudiantes' ")
-        registrar_estudiante()
-    case "2":
-        print("\nEscogio 'Agregar cursos con nota' ")
-        agregar_notas()
-    case "3":
-        print("\nEscogio 'Consultar estudiantes' ")
+    if buscador in estudiantes:
+        print(f"\nNombre del estudiante: {estudiantes[buscador]['Nombre']}")
+        print(f"Carrera: {estudiantes[buscador]['Carrera']}")
+
+        if not estudiantes[buscador]["Curso"]:
+            print("No se ha encontrado ningun curso")
+
+        else:
+            for curso_nom, valor in estudiantes[buscador]['Curso'].items():
+                print(f"Curso: {curso_nom}, nota: {valor['Nota']}")
+            promedio=len(estudiantes[buscador]['Curso'])
+
+
+while True:
+
+    print("\n---PORTAL ACADÉMICO---")
+    print("\n1. Agregar estudiante")
+    print("2. Agregar curso con nota")
+    print("3. Consultar estudiante")
+    print("4. Calcular promedio")
+    print("5. Verificación de aprobación")
+    print("6. Mostrar todos los registros")
+    print("7. Salir")
+
+    opcion=input("Seleccione una opción: ")
+
+    match opcion:
+        case "1":
+            print("\nEscogio 'Agregar estudiantes' ")
+            registrar_estudiante()
+            print("Estudiante agregado exitosamente")
+
+        case "2":
+            print("\nEscogio 'Agregar cursos con nota' ")
+            todo_notas()
+            print("Curso agregado exitosamente")
+
+        case "3":
+            print("\nEscogio 'Consultar estudiantes' ")
+            id_pa_buscar=input("Ingrese su ID de estudiante: ")
+            consultar_est(id_pa_buscar)
+
 
 
 
